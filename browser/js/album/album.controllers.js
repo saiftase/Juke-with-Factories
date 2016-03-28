84be4d322f93d1dac2209108636bb898877431de
distres117 <omcrobbie@gmail.com>
2016-03-28 19:43:44 -0400

    refactoring

--- browser/js/album/album.controllers.js
+++ browser/js/album/album.controllers.js
@@ -1,6 +1,6 @@
 //'use strict';
 
-juke.controller('AlbumCtrl', function($scope, $http, $rootScope, $log, StatsFactory, HttpFactory) {
+juke.controller('AlbumCtrl', function($scope, $http, $rootScope, $log, StatsFactory, PlayerFactory, HttpFactory) {
 
   // load our initial data
   HttpFactory.fetchAll()
@@ -24,38 +24,29 @@ juke.controller('AlbumCtrl', function($scope, $http, $rootScope, $log, StatsFact
 
   // main toggle
   $scope.toggle = function (song) {
-    if ($scope.playing && song === $scope.currentSong) {
-      $rootScope.$broadcast('pause');
-    } else $rootScope.$broadcast('play', song);
+    if (PlayerFactory.isPlaying() && song === PlayerFactory.getCurrentSong()) {
+      pause();
+    } else
+      play(null, song);
   };
 
-  // incoming events (from Player, toggle, or skip)
-  $scope.$on('pause', pause);
-  $scope.$on('play', play);
-  $scope.$on('next', next);
-  $scope.$on('prev', prev);
+  $scope.getCurrentSong = function(){
+    return PlayerFactory.getCurrentSong();
+  };
+
+  $scope.isPlaying = function(){
+    return PlayerFactory.isPlaying();
+  };
 
   // functionality
   function pause () {
-    $scope.playing = false;
+    PlayerFactory.pause();
   }
   function play (event, song) {
-    $scope.playing = true;
-    $scope.currentSong = song;
+    PlayerFactory.start(song);
   }
 
-  // a "true" modulo that wraps negative to the top of the range
-  function mod (num, m) { return ((num % m) + m) % m; }
-
-  // jump `interval` spots in album (negative to go back, default +1)
-  function skip (interval) {
-    if (!$scope.currentSong) return;
-    var index = $scope.currentSong.albumIndex;
-    index = mod( (index + (interval || 1)), $scope.album.songs.length );
-    $scope.currentSong = $scope.album.songs[index];
-    if ($scope.playing) $rootScope.$broadcast('play', $scope.currentSong);
-  }
-  function next () { skip(1); }
-  function prev () { skip(-1); }
+  function next () { PlayerFactory.next(); }
+  function prev () { PlayerFactory.previous(); }
 
 });
--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -1,5 +1,70 @@
-'use strict';
-
 juke.factory('PlayerFactory', function(){
   // non-UI logic in here
+  var audio = document.createElement('audio');
+  var fac = {};
+  var currentSong = null;
+  var playing = false;
+  var songs = null;
+  var currentIndex;
+  var progress =0;
+
+  audio.addEventListener('timeupdate', function () {
+    progress = audio.currentTime / audio.duration * 100;
+  });
+  audio.addEventListener('ended', function () {
+    fac.next();
+  });
+
+  fac.start = function(song, songlist){
+    if (songlist){
+      songs = songlist;
+      currentIndex = songlist.indexOf(song);
+    }
+    fac.pause();
+    playing = true;
+    if (song === currentSong) return audio.play();
+    currentSong = song;
+    audio.src = song.audioUrl;
+    audio.load();
+    progress = 0;
+    audio.play();
+
+  };
+
+  fac.pause = function(){
+    audio.pause();
+    playing = false;
+
+  };
+
+  fac.resume = function(){
+    audio.play();
+    playing = true;
+  };
+
+  fac.isPlaying = function(){
+    return playing;
+  };
+
+  fac.getCurrentSong = function(){
+    return currentSong;
+  };
+
+  fac.next = function(){
+    if (songs)
+      fac.start(currentIndex + 1 > songs.length -1 ? songs[0] : songs[currentIndex+1]);
+  };
+
+  fac.previous = function(){
+    if (songs)
+      fac.start(currentIndex -1 < 0 ? songs[songs.length -1] : songs[currentIndex - 1]);
+  };
+
+  fac.getProgress = function(){
+    return progress;
+  };
+
+
+
+  return fac;
 });
